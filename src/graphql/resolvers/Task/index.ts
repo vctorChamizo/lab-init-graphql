4cdd5032776bc9a4e20841d790bf8039acdc3107
vctorChamizo <victor.chamizo96@gmail.com>
2021-03-09 17:10:45 +0100

    fix: compiler bug

--- src/graphql/resolvers/Task/index.ts
+++ src/graphql/resolvers/Task/index.ts
@@ -5,14 +5,14 @@ import { findTasksQuery, findTaskByIdQuery } from './task.query'
 
 import { ITask } from '@interfaces'
 
-const status = (task: ITask): string => {
-  return `Name: ${task.name} - Status: ${
-    task.completed ? 'Completada' : 'Incompleta'
-  }`
-}
+// const status = (task: ITask): string => {
+//   return `Name: ${task.name} - Status: ${
+//     task.completed ? 'Completada' : 'Incompleta'
+//   }`
+// }
 
 export const taskResolver: IResolvers = {
   Query: { findTasksQuery, findTaskByIdQuery },
-  Mutation: { addTaskMutation },
-  Task: { status }
+  Mutation: { addTaskMutation }
+  // Task: { status }
 }
