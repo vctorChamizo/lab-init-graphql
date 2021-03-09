f3e61ea591511cf740deaddc63e662cb4f7e55bf
vctorChamizo <victor.chamizo96@gmail.com>
2021-03-09 14:40:34 +0100

    feat: prettier config

--- src/app.ts
+++ src/app.ts
@@ -1,40 +1,40 @@
-import express from "express";
-import helmet from "helmet";
-import { ApolloServer, makeExecutableSchema } from "apollo-server-express";
+import express from 'express'
+import helmet from 'helmet'
+import { ApolloServer, makeExecutableSchema } from 'apollo-server-express'
 
-import resolvers from "./graphql/resolvers";
-import typeDefs from "./graphql/types";
-import * as database from "./db/connector";
-import context from "./context";
-import formatError from "./errors";
+import resolvers from './graphql/resolvers'
+import typeDefs from './graphql/types'
+import * as database from './db/connector'
+import context from './context'
+import formatError from './errors'
 
-import logger from "@log";
-import { NODE_ENV, API_HOST, PORT, API_URL } from "@constants";
-import { Enviroments } from "@enums";
+import logger from '@log'
+import { NODE_ENV, API_HOST, PORT, API_URL } from '@constants'
+import { Enviroments } from '@enums'
 
-const app = express();
+const app = express()
 
-app.use(helmet());
+app.use(helmet())
 
-database.connect();
+database.connect()
 
 const schema = makeExecutableSchema({
   typeDefs,
-  resolvers,
-});
+  resolvers
+})
 
 const server = new ApolloServer({
   schema,
   playground: true,
   debug: NODE_ENV == Enviroments.SANDBOX,
   context,
-  formatError,
-});
+  formatError
+})
 
-server.applyMiddleware({ app, path: API_URL });
+server.applyMiddleware({ app, path: API_URL })
 
 app.listen(PORT, () => {
   logger.info(
     `GraphQL Server running at ${API_HOST}:${PORT}${server.graphqlPath}`
-  );
-});
+  )
+})
