c91cdff99d433b3c387f5c4fe7a5e83639b5a416
Víctor Chamizo <victor.chamizo96@gmail.com>
2021-03-24 10:04:39 +0100

    Update index.ts

--- src/graphql/resolvers/user/index.ts
+++ src/graphql/resolvers/user/index.ts
@@ -2,6 +2,11 @@ import { IResolvers } from 'apollo-server-express'
 
 import { findUsersQuery, findUserByIdQuery } from './user.query'
 
+const id = (user: IUser): string => {
+  return String(user.id)
+}
+
 export const userResolver: IResolvers = {
   Query: { findUsersQuery, findUserByIdQuery }
+  User: { id }
 }
